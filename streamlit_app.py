1e39f08d54eb69ac479d4d25d0b0ac5b5b03eb7f
GGeys <82289341+GGeys@users.noreply.github.com>
2024-04-08 19:47:58 +0200

    Update streamlit_app.py
    changes to search terms

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'),
+st.stop()
 
 Name_on_order = st.text_input('Name on smoothie:')
 
